77693a8809dc10871987d491ce4d2251be2bb133
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-23 21:45:53 +0530

    Create Header components : Navbars

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,13 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import { Navbar, TextInput, Button } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 
 const Header = () => {
+  // For Active Page indicator using path
+  const path = useLocation().pathname;
   return (
     <div>
       <Navbar className='border-b-2'>
@@ -38,6 +40,17 @@ const Header = () => {
           </Link>
           <Navbar.Toggle />
         </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === '/'} as={'div'}>
+            <Link to={'/'}>Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/about'} as={'div'}>
+            <Link to={'/about'}>About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/projects'} as={'div'}>
+            <Link to={'/projects'}>Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
