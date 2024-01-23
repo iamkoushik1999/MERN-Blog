1eaac32e8bbf0f9ffd56b8eb4c84b0c6722ec7ba
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-23 21:41:02 +0530

    Create Header components : Home Link

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+
+import { Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar className='border-b-2'>
+        <Link
+          to={'/'}
+          className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Koushik's
+          </span>
+          Blog
+        </Link>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
