df636146d7bde2d18b6ef0c3234b158c1f2b74c7
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-24 20:04:02 +0530

    Create Header components: Sign In Outline

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -36,7 +36,9 @@ const Header = () => {
             <FaMoon />
           </Button>
           <Link to={'/sign-in'}>
-            <Button gradientDuoTone={'purpleToBlue'}>Sign in</Button>
+            <Button gradientDuoTone={'purpleToBlue'} outline>
+              Sign in
+            </Button>
           </Link>
           <Navbar.Toggle />
         </div>
