01c98d650d7a62b7218e28614b636a3863069189
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-24 20:38:59 +0530

    Create user model

---
+++ api/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
