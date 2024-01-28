d7ea1354c753ce443289b037d739051452e33f3c
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-28 20:54:14 +0530

    Create signin API route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,8 +1,11 @@
 // Packages
 const bcryptjs = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
 // Models
 const userModel = require('../models/userModel');
+// ENV
+const { SECRET_KEY } = process.env;
 
 // ----------------------------------------------------------- Auth -----------------------------------------------------------
 
@@ -46,4 +49,37 @@ exports.signup = asyncHandler(async (req, res, next) => {
     res.status(500);
     throw new Error('Internal server error');
   }
+});
+
+// POST
+// Sign In
+exports.signin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === '' || password === '') {
+    res.status(400);
+    throw new Error('All Fields are required');
+  }
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    res.status(404);
+    throw new Error('Invalid Credentials');
+  }
+
+  const { password: pass, ...userData } = user._doc;
+
+  const confirmPassword = bcryptjs.compareSync(password, user.password);
+  if (!confirmPassword) {
+    res.status(404);
+    throw new Error('Invalid Credentials');
+  }
+
+  const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
+
+  res
+    .status(200)
+    .cookie('access_token', token, {
+      httpOnly: true,
+    })
+    .json({ message: 'Signin successful', userData });
 });
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-const { signup } = require('../controllers/authController');
+const { signup, signin } = require('../controllers/authController');
 
 // ----------------------------------------------------------- Auth -----------------------------------------------------------
 // POST
 // Sign Up
 router.post('/signup', signup);
+
+// POST
+// Sign In
+router.post('/signin', signin);
 
 module.exports = router;
