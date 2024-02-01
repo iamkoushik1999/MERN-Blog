ea23f5dd799c97000639b2d04e5771ca918740d1
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-02-01 23:36:06 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,18 @@
 import { Navbar, TextInput, Button, Dropdown, Avatar } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 const Header = () => {
   // For Active Page indicator using path
   const path = useLocation().pathname;
 
   const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+
+  const dispatch = useDispatch();
   return (
     <div>
       <Navbar className='border-b-2'>
@@ -35,8 +39,12 @@ const Header = () => {
           <AiOutlineSearch />
         </Button>
         <div className='flex gap-2 md:order-2'>
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon />
+          <Button
+            className='w-12 h-10 hidden sm:inline'
+            color='gray'
+            pill
+            onClick={() => dispatch(toggleTheme())}>
+            {theme === 'light' ? <FaMoon /> : <FaSun />}
           </Button>
           {currentUser ? (
             <Dropdown
