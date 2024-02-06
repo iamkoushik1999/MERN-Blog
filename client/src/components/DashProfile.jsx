6f7791b8e04987c8db06e3ec60c59119ec865973
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-02-06 21:55:06 +0530

    DashProfile isAdmin & Loading

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -23,9 +23,10 @@ import {
   signoutSuccess,
 } from '../redux/user/userSlice';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 const DashProfile = () => {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const filePickerRef = useRef();
@@ -237,9 +238,23 @@ const DashProfile = () => {
           placeholder='Password'
           onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone={'purpleToBlue'} outline>
-          Update
+        <Button
+          type='submit'
+          gradientDuoTone={'purpleToBlue'}
+          outline
+          disabled={loading || imageFileUploading}>
+          {loading ? 'loading...' : 'Update'}
         </Button>
+        {currentUser.userData.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone={'purpleToPink'}
+              className='w-full'>
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer' onClick={() => setShowModal(true)}>
