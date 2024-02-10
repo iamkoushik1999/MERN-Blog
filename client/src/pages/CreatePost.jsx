2742ccc266f1f9a27b83cf43abefe4d468a716ca
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-02-10 23:06:31 +0530

    Complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -12,12 +12,16 @@ import {
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  // console.log('formData', formData);
+  const [publishError, setPublishError] = useState(null);
+  const navigate = useNavigate();
 
   const handleUploadImage = async () => {
     try {
@@ -38,8 +42,8 @@ const CreatePost = () => {
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          console.log(error);
-          setImageUploadError('Could not upload image');
+          // console.log(error);
+          setImageUploadError('Could not upload image', error);
           setImageUploadProgress(null);
         },
         () => {
@@ -53,14 +57,38 @@ const CreatePost = () => {
     } catch (error) {
       setImageUploadError('Image upload failed');
       setImageUploadProgress(null);
-      console.log(error);
+      // console.log(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('api/v1/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.post.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
     }
   };
 
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create Post</h1>
-      <form className='flex flex-col gap-4 '>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -68,8 +96,14 @@ const CreatePost = () => {
             required
             id='title'
             className='flex-1'
+            onChange={(e) => {
+              setFormData({ ...formData, title: e.target.value });
+            }}
           />
-          <Select>
+          <Select
+            onChange={(e) => {
+              setFormData({ ...formData, category: e.target.value });
+            }}>
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
             <option value='react'>React.js</option>
@@ -116,10 +150,18 @@ const CreatePost = () => {
           placeholder='Write something...'
           className='h-72 mb-12'
           required
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
         />
         <Button type='submit' gradientDuoTone={'purpleToPink'}>
           Publish
         </Button>
+        {publishError && (
+          <Alert className='mt-5' color={'failure'}>
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   );
